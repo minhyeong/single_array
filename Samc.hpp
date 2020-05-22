21eea4e35cc6d3d7344a51ed11c4ab4f82166eb2
minhyeong.toshita <minhyeong.toshita@gmail.com>
2020-05-22 06:01:16 -0700

    a

--- Samc.hpp
+++ Samc.hpp
@@ -214,6 +214,7 @@ _SamcImpl<CodeType>::_SamcImpl(const string_array_explorer<Iter>& explorer) {
   // new > 04/20 imamura
   std::cout << "storage_ : " << storage_.size() * sizeof(char_type) << " [Byte]" << std::endl;
   std::cout << "code_table_ : " << code_table_.size() * sizeof(std::array<code_type, kAlphabetSize>) << " [Byte]" << std::endl;
+  std::cout << "Single Array Size : " << storage_.size() * sizeof(char_type) + code_table_.size() * sizeof(std::array<code_type, kAlphabetSize>) << " [Byte]" << std::endl;
 }
 
 template <typename CodeType>
