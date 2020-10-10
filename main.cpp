9bf1eeae7ae16d678c9ad25d46a7b4a745a0c45e
minhyeong <minhyeong.toshita@gmail.com>
2020-10-10 21:29:32 +0900

    add ignore

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 
 //#include "Samc.hpp"
-#include "Rank_Samc.hpp" // 
+#include "Rank_Samc.hpp"
 //#include "rank_samc_0908.hpp"
-#include "sdsl/rrr_vector.hpp"
+//#include "sdsl/rrr_vector.hpp"
 #include "SuccinctBitVector.hpp"
 
-#define FILE_PATH "../corpus/Japan_Postal_Code.txt"
+//#define FILE_PATH "../corpus/Japan_Postal_Code.txt"
+#define FILE_PATH "../corpus/dna.100MB"
 //#define FILE_PATH "../corpus/en.txt"
 //#define FILE_PATH "../corpus/test_postal_code.txt"
 
